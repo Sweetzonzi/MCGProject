56dd9f6f66130c7f0551dd91b3f35c97b8599b49
Sweetzonzi <1368003448@qq.com>
2023-06-05 10:18:30 +0800

    竹子和竹笋

--- src/main/java/moe/gensoukyo/mcgproject/block/MCGNatureBlocks.java
+++ src/main/java/moe/gensoukyo/mcgproject/block/MCGNatureBlocks.java
@@ -17,22 +17,22 @@ public class MCGNatureBlocks{
     //方块----------------------------------------------------
     //爬山虎
     public static final RegistryObject<Block> IVY = BlockRegistry.registerBlock("ivy",
-            ()->new IvyBlock(BlockBehaviour.Properties.of(Material.REPLACEABLE_PLANT).noCollission()), MCGTab.MCG_NATURE);
+            ()->new IvyBlock(BlockBehaviour.Properties.of(Material.REPLACEABLE_PLANT).noCollission().sound(SoundType.GRASS)), MCGTab.MCG_NATURE);
     //三叶草
     public static final RegistryObject<Block> CLOVER = BlockRegistry.registerBlock("clover",
-            ()->new Block(BlockBehaviour.Properties.of(Material.REPLACEABLE_PLANT).noCollission()), MCGTab.MCG_NATURE);
+            ()->new Block(BlockBehaviour.Properties.of(Material.REPLACEABLE_PLANT).noCollission().sound(SoundType.GRASS)), MCGTab.MCG_NATURE);
     //带四叶草三叶草
     public static final RegistryObject<Block> LUCKY_CLOVER = BlockRegistry.registerBlock("lucky_clover",
-            ()->new Block(BlockBehaviour.Properties.of(Material.REPLACEABLE_PLANT).noCollission()), MCGTab.MCG_NATURE);
+            ()->new Block(BlockBehaviour.Properties.of(Material.REPLACEABLE_PLANT).noCollission().sound(SoundType.GRASS)), MCGTab.MCG_NATURE);
     //水稻苗
     public static final RegistryObject<Block> PADDY = BlockRegistry.registerBlock("paddy",
-            ()->new Block(BlockBehaviour.Properties.of(Material.PLANT).noCollission().noOcclusion()), MCGTab.MCG_NATURE);
+            ()->new Block(BlockBehaviour.Properties.of(Material.PLANT).noCollission().noOcclusion().sound(SoundType.CROP)), MCGTab.MCG_NATURE);
     //稀疏水稻苗
     public static final RegistryObject<Block> PADDY_SPARSE = BlockRegistry.registerBlock("paddy_sparse",
-            ()->new Block(BlockBehaviour.Properties.of(Material.PLANT).noCollission().noOcclusion()), MCGTab.MCG_NATURE);
+            ()->new Block(BlockBehaviour.Properties.of(Material.PLANT).noCollission().noOcclusion().sound(SoundType.CROP)), MCGTab.MCG_NATURE);
     //密植水稻苗
     public static final RegistryObject<Block> PADDY_DENSE = BlockRegistry.registerBlock("paddy_dense",
-            ()->new Block(BlockBehaviour.Properties.of(Material.PLANT).noCollission().noOcclusion()), MCGTab.MCG_NATURE);
+            ()->new Block(BlockBehaviour.Properties.of(Material.PLANT).noCollission().noOcclusion().sound(SoundType.CROP)), MCGTab.MCG_NATURE);
     //水田
     public static final RegistryObject<Block> WATER_FARMLAND = BlockRegistry.registerBlock("water_farmland",
             ()->new WaterFarmBlock(BlockBehaviour.Properties.of(Material.DIRT).randomTicks().strength(0.6F).sound(SoundType.GRAVEL)), MCGTab.MCG_NATURE);
@@ -47,8 +47,8 @@ public class MCGNatureBlocks{
             ()->new BambooBlock(BlockBehaviour.Properties.of(Material.PLANT, MaterialColor.PLANT).sound(SoundType.BAMBOO).dynamicShape().noOcclusion().randomTicks()), MCGTab.MCG_NATURE);
     //嫩竹笋
     public static final RegistryObject<Block> BAMBOO_SHOOT_FRESH = BlockRegistry.registerBlock("bamboo_shoot_fresh",
-            ()->new Block(BlockBehaviour.Properties.of(Material.PLANT).noCollission().noOcclusion()), MCGTab.MCG_NATURE);
+            ()->new Block(BlockBehaviour.Properties.of(Material.PLANT).noCollission().noOcclusion().sound(SoundType.BAMBOO_SAPLING)), MCGTab.MCG_NATURE);
     //老竹笋
     public static final RegistryObject<Block> BAMBOO_SHOOT_OLD = BlockRegistry.registerBlock("bamboo_shoot_old",
-            ()->new Block(BlockBehaviour.Properties.of(Material.PLANT).noCollission().noOcclusion()), MCGTab.MCG_NATURE);
+            ()->new Block(BlockBehaviour.Properties.of(Material.PLANT).noCollission().noOcclusion().sound(SoundType.BAMBOO_SAPLING)), MCGTab.MCG_NATURE);
 }
